ee73b91b23ea29cf76739de9be1d60864ab317a1
RaoNaila <160322743+RaoNaila@users.noreply.github.com>
2024-05-19 11:13:24 -0700

    my todo app

---
+++ main.ts
@@ -0,0 +1,46 @@
+import inquirer from "inquirer"
+
+
+//variable
+
+const currency:any = {
+
+    USD:1,//base currency
+    PKR:280,
+    SR :77,
+    EUR:0.91,
+    INR:74.57,
+
+};
+let userAnswer = await inquirer.prompt(
+    [
+        {
+            name:"from",
+            message:"which currency you want to convert",
+            type:"list",
+            choices:["USD","PKR","SR","EUR","INR"] 
+        },
+        {
+            name:"to",
+            message:"select in which you want to convert",
+            type:"list",
+            choices:["USD","PKR","SR","EUR","INR"] 
+        },
+        {
+            name:"amount",
+            message:"Enter your amount",
+            type:"number",
+        }
+    ]
+);
+
+let fromamount=currency[userAnswer.from];
+let toamount=currency[userAnswer.to];
+let amount=userAnswer.amount;
+let baseAmount= amount/fromamount;
+let convertAmount=baseAmount*toamount
+
+console.log(convertAmount);
+
+
+
